6f53f6f7e6b6855e402e03ee886822954daa9d30
abhay-sen <abhaysen0502@gmail.com>
2024-09-29 19:27:13 +0530

    fronted for auth

--- gpg-project/src/App.jsx
+++ gpg-project/src/App.jsx
@@ -3,33 +3,69 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useCallback } from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import SuperTokens, { SuperTokensWrapper } from "supertokens-auth-react";
+
+import { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react/ui";
+import { ThirdPartyPreBuiltUI } from "supertokens-auth-react/recipe/thirdparty/prebuiltui";
+import { EmailPasswordPreBuiltUI } from "supertokens-auth-react/recipe/emailpassword/prebuiltui";
+import * as reactRouterDom from "react-router-dom";
+
+import ThirdParty, {
+  Github,
+  Google,
+  Facebook,
+  Apple,
+} from "supertokens-auth-react/recipe/thirdparty";
+import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
+import Session from "supertokens-auth-react/recipe/session";
+
+SuperTokens.init({
+  appInfo: {
+    // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
+    appName: "AlumniDekho",
+    apiDomain: "http://localhost:4050",
+    websiteDomain: "http://localhost:5173",
+    apiBasePath: "/auth",
+    websiteBasePath: "/auth",
+  },
+  recipeList: [
+    ThirdParty.init({
+      signInAndUpFeature: {
+        providers: [
+          Github.init(),
+          Google.init(),
+          Facebook.init(),
+          Apple.init(),
+        ],
+      },
+    }),
+    EmailPassword.init(),
+    Session.init(),
+  ],
+});
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Manas</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <SuperTokensWrapper>
+      <BrowserRouter>
+        <Routes>
+          {/*This renders the login UI on the /auth route*/}
+          
+            {getSuperTokensRoutesForReactRouterDom(reactRouterDom, [
+              ThirdPartyPreBuiltUI,
+              EmailPasswordPreBuiltUI,
+            ])}
+         
+
+          {/*Your app routes*/}
+        </Routes>
+      </BrowserRouter>
+    </SuperTokensWrapper>
+  );
 }
 
 export default App
