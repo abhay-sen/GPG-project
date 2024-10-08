96fdddf9fbd0f4aeccd16725b308620c5ffb8f87
Manas Chilamwar <62495446+manas-chilamwar@users.noreply.github.com>
2024-10-08 22:48:32 +0530

    changes

--- gpg-project/src/firebase.jsx
+++ gpg-project/src/firebase.jsx
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCsLkA2-oUJFaCW7kA5tkMuqemDPfS9INw",
   authDomain: "alumnidekho-2fd65.firebaseapp.com",
@@ -16,6 +12,5 @@ const firebaseConfig = {
   measurementId: "G-WSWB08XKQV"
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
